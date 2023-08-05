da1bc8bf6da678b09902c282c779fad2f33e04e9
kumar11jr <kumar11jr@gmail.com>
2023-08-05 22:20:33 +0530

    face detection using python

---
+++ Face_detect.py
@@ -0,0 +1,20 @@
+import cv2
+
+face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+img = cv2.imread('dv.png')
+
+
+gray_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+
+face_coordinates = face_data.detectMultiScale(gray_img)
+for (x,y,w,h) in face_coordinates:
+    cv2.rectangle(img, (x,y),(w+x,h+y),(0,255,0),2)
+
+print(face_coordinates)
+cv2.imshow('prabhat face detection',img)
+
+cv2.waitKey()
+
+print("code completed")
---
+++ face_detect_video.py
@@ -0,0 +1,18 @@
+import cv2
+
+face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+webcam = cv2.VideoCapture(0)
+
+while True:
+    successful_frame_read,frame = webcam.read()
+    
+    gray_img = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    face_coordinates = face_data.detectMultiScale(gray_img)
+    for (x,y,w,h) in face_coordinates:
+        cv2.rectangle(frame, (x,y),(w+x,h+y),(0,255,0),2)
+    cv2.imshow('prabhat face detection',frame)
+    cv2.waitKey(1)
+    
+    
+webcam.release()
