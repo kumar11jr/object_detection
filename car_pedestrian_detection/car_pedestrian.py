c4193624c14af93806efd6d5ea990edc8665a7e9
kumar11jr <kumar11jr@gmail.com>
2023-08-06 11:57:49 +0530

    day 2

--- car_pedestrian_detection/car_pedestrian.py
+++ car_pedestrian_detection/car_pedestrian.py
@@ -2,9 +2,9 @@ import cv2
 
 # img = cv2.imread('car.jpg')
 
-video = cv2.VideoCapture('dataset_video1.avi')
+video = cv2.VideoCapture('car-and-pedestrian-video0.mp4')
 car_cascade = cv2.CascadeClassifier('cars.xml')
-pedestrian_cascade = cv2.CascadeClassifier('cars.xml')
+pedestrian_cascade = cv2.CascadeClassifier('haarcascade_fullbody.xml')
 
 while True:
     (read_successful,frame) = video.read()
@@ -14,13 +14,19 @@ while True:
         break
     
     cars = car_cascade.detectMultiScale(gray, 1.1, 1)
+    pedestrian = car_cascade.detectMultiScale(gray, 1.1, 1)
 
     for (x,y,w,h) in cars:
         cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)      
     
+    for (x,y,w,h) in pedestrian:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+    
     cv2.imshow('video', frame)
     
-    cv2.waitKey(1)
+    if cv2.waitKey(25) & 0xFF == 27:  # Press 'Esc' key to exit
+        break
     
 
+video.release()
 cv2.destroyAllWindows()
